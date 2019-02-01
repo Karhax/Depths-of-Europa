d36cc2ed68e99c506c7075acaaa2e5630666e30b
Filip Petersson <a18filpe@student.his.se>
2019-02-01 18:01:22 +0100

    Added ship movement, camera follow + effects, Basic ship lighting

---
+++ Depths of Europa/Assets/Filip/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField, Range(0, 5)] float _cameraFollowSpeed = 1;
+    [SerializeField, Range(1, 5)] float _cameraGoBackAmount = 2;
+    [SerializeField] float _forwardCameraZoomOutAmount = 2;
+    [SerializeField] float _backwardCameraZoomOutAmount = 1.2f;
+    [SerializeField] Transform _player;
+
+    Camera _thisCamera;
+    Rigidbody2D _playerRigidBody;
+
+    float _originalCameraSize;
+
+    private void Awake()
+    {
+        _thisCamera = GetComponent<Camera>();
+        _playerRigidBody = _player.GetComponent<Rigidbody2D>();
+
+        _originalCameraSize = _thisCamera.orthographicSize;
+    }
+
+    private void LateUpdate()
+    {
+        FollowPlayer();
+        ZoomOutDueToSpeed();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 newPosition = _player.transform.position + (Vector3)_playerRigidBody.velocity * _cameraGoBackAmount;
+        newPosition.z = transform.position.z;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * _cameraFollowSpeed);
+    }
+
+    private void ZoomOutDueToSpeed()
+    {
+        float dotProduct = Vector3.Dot(_playerRigidBody.velocity, _player.up);
+        float newSize = _originalCameraSize;
+
+        if (dotProduct >= 0)
+            newSize += dotProduct * _forwardCameraZoomOutAmount;
+        else
+            newSize += -dotProduct * _backwardCameraZoomOutAmount;
+
+        _thisCamera.orthographicSize = newSize;
+    }
+}
---
+++ Depths of Europa/Assets/Filip/Scripts/Ship/MoveShip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShip : MonoBehaviour
+{
+    [SerializeField, Range(0, 250)] float _turnSpeed = 60f;
+    [SerializeField, Range(0, 3)] float _baseForwardSpeed = 0.55f;
+    [SerializeField, Range(0, 3)] float _baseBackWardSpeed = 0.25f;
+    [SerializeField, Range(0, 3)] float _accelerationModifier = 0.55f;
+
+    Rigidbody2D thisRigidbody;
+
+    private void Awake()
+    {
+        thisRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+
+        float dotProduct = Vector3.Dot(thisRigidbody.velocity, transform.up);
+
+        if (turn != 0)
+            thisRigidbody.AddTorque(turn * -dotProduct * Time.deltaTime * _turnSpeed);
+
+        if (move != 0)
+        {
+            if (move > 0)
+                SetNewSpeed(move, _baseForwardSpeed, dotProduct);
+            else
+                SetNewSpeed(move, _baseBackWardSpeed, dotProduct);
+
+        }
+    }
+
+    private void SetNewSpeed(float move, float speedType, float dotProduct)
+    {
+        thisRigidbody.velocity += (Vector2)transform.up * move * Time.deltaTime * (speedType + Mathf.Abs(dotProduct) * _accelerationModifier);
+    }
+
+}
