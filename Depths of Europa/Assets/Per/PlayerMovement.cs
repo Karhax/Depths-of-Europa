ca357413e6c43f7bd2d33578526216f2d3163e46
Per Bengtsson <perbengtsson@live.se>
2019-02-04 14:55:25 +0100

    Reworked player movement and added ladder interaction.
    Signed-off-by: Per Bengtsson

--- Depths of Europa/Assets/Per/PlayerMovement.cs
+++ Depths of Europa/Assets/Per/PlayerMovement.cs
@@ -3,36 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    
+    [SerializeField] private float _moveSpeed = 150f;
+    private Vector2 _currentSpeed = new Vector2(0, 0);
 
-    [SerializeField] private float _moveAcceleration = 2.5f;
-    [SerializeField] private float _moveSpeedMax = 3f;
-    private float _moveForce = 0f;
+    [SerializeField] private float _ladderMoveSpeed = 120f;
+    private bool _onLadder = false;
+
+    private float _normalGravity = 0;
 
     private Rigidbody2D _rigidbody;
 
 	private void Start ()
     {
         _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _normalGravity = _rigidbody.gravityScale;
 	}
-
-    private void FixedUpdate()
-    {
-        // Add force to rigidbody
-        _rigidbody.AddForce(new Vector2(_moveForce, 0));
-        LimitSpeed();   
-    }
 	
 	private void Update () {
-        // Take input and calculate force to be added
-        _moveForce = _moveAcceleration * Input.GetAxisRaw(Statics.GameInput.HORIZONTAL) * Time.deltaTime;
+        // Handle input and movement for X Axis
+        int directionMultiplier = (int)Input.GetAxisRaw(Statics.GameInput.HORIZONTAL);
+        CalculateNewSpeedX(directionMultiplier);
+
+        // if on ladder, handle input and movement for Y Axis
+        if (_onLadder)
+        {
+            directionMultiplier = (int)Input.GetAxisRaw(Statics.GameInput.VERTICAL);
+            CalculateNewSpeedY(directionMultiplier);
+        }
+
+
+        _rigidbody.velocity = _currentSpeed;
 	}
 
-    private void LimitSpeed()
+    private void CalculateNewSpeedX(int directionMultiplier)
     {
-        float absoluteVelocity = Mathf.Abs(_rigidbody.velocity.x);
-        if (absoluteVelocity > _moveSpeedMax)
+        if (directionMultiplier == 0)
         {
-            _rigidbody.velocity = new Vector2(_moveSpeedMax * Mathf.Sign(_rigidbody.velocity.x), 0);
+            // No direction means character should stop moving.
+            _currentSpeed = new Vector2(0, _rigidbody.velocity.y);
+        }
+        else
+        {
+            if (Mathf.Sign(_currentSpeed.x) != Mathf.Sign(directionMultiplier))
+            {
+                // A change in direction was detected. Set speed to a low speed in the new direction
+                _currentSpeed = new Vector2(_moveSpeed / 10 * directionMultiplier * Time.deltaTime, _rigidbody.velocity.y);
+            }
+            else
+            {
+                // For continuous movement in a direction, have speed be the predefined speed
+                _currentSpeed = new Vector2(_moveSpeed * directionMultiplier * Time.deltaTime, _rigidbody.velocity.y);
+            }
+        }
+    }
+    private void CalculateNewSpeedY(int directionMultiplier)
+    {
+        if (directionMultiplier == 0)
+        {
+            // No direction means character should stop moving.
+            _currentSpeed = new Vector2(_currentSpeed.x, 0);
+        }
+        else
+        {
+            if (Mathf.Sign(_currentSpeed.y) != Mathf.Sign(directionMultiplier))
+            {
+                // A change in direction was detected. Set speed to a low speed in the new direction
+                _currentSpeed = new Vector2(_currentSpeed.x, _ladderMoveSpeed / 10 * directionMultiplier * Time.deltaTime);
+            }
+            else
+            {
+                // For continuous movement in a direction, have speed be the predefined speed
+                _currentSpeed = new Vector2(_currentSpeed.x, _ladderMoveSpeed * directionMultiplier * Time.deltaTime);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            _onLadder = true;
+            _rigidbody.gravityScale = 0;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ladder"))
+        {
+            _onLadder = false;
+            _rigidbody.gravityScale = _normalGravity;
         }
     }
 }
