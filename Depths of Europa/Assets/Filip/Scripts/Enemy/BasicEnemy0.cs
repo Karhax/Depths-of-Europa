ead1022ffaaa3954375efd9d9388810f0f1dbbe4
Filip Petersson <a18filpe@student.his.se>
2019-02-04 17:28:33 +0100

    Flares, VERY basic enemy movement
    Signed-off-by: Filip Petersson <a18filpe@student.his.se>

---
+++ Depths of Europa/Assets/Filip/Scripts/Enemy/BasicEnemy0.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy0 : MonoBehaviour
+{
+    [SerializeField] EnemyIdleBase _idleState;
+    [SerializeField] EnemyAttackBase _attackState;
+    [SerializeField] EnemyEscapeBase _escapeState;
+
+    EnemyStateBase _currentState;
+
+    private void Awake()
+    {
+        _idleState.SetUp(this);
+        _attackState.SetUp(this);
+        _escapeState.SetUp(this);
+
+        ChangeState(_idleState);
+    }
+
+    private void Update()
+    {
+        ChangeState(_currentState.Update());
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        _currentState.OnTriggerStay(other);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        _currentState.OnTriggerExit(other);
+    }
+
+    private void ChangeState(EnemyStates state)
+    {
+        switch (state)
+        {
+            case (EnemyStates.STAY): { return; }
+            case (EnemyStates.IDLE): { ChangeState(_idleState); break; }
+            case (EnemyStates.ESCAPE): { ChangeState(_escapeState); break; }
+            case (EnemyStates.ATTACK): { ChangeState(_attackState); break; }
+
+            default: { throw new System.Exception("This enemy state is not fully implemented! " + this + " " + gameObject.name); }
+        }
+    }
+
+    private void ChangeState(EnemyStateBase state)
+    {
+        if (_currentState != null)
+            _currentState.ExitState();
+
+        _currentState = state;
+
+        _currentState.EnterState();
+    }
+}
---
+++ Depths of Europa/Assets/Filip/Scripts/Enemy/EnemyEscapeBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyEscapeBase : EnemyStateBase
+{
+    public override void EnterState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExitState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override EnemyStates Update()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override EnemyStates OnTriggerStay(Collider2D other)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override EnemyStates OnTriggerExit(Collider2D other)
+    {
+        throw new System.NotImplementedException();
+    }
+}
