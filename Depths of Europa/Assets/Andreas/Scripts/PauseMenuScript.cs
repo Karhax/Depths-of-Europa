da3c7d211149ecb904a55320d1ae225305140164
Andreas Söderlund <lets.play.ksp@gmail.com>
2019-02-05 14:48:21 +0100

    Added pause and fixed bug in timer script

---
+++ Depths of Europa/Assets/Andreas/Scripts/PauseMenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Statics;
+
+public class PauseMenuScript : MonoBehaviour {
+
+
+    [SerializeField] bool _isPaused = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown(GameInput.Cancel))
+        {
+            SetPause();
+        }
+	}
+    //TODO: Probably better to store the start tiem scale somewhere instead of assuming 1
+    
+    private void SetPause()
+    {
+        _isPaused = !_isPaused;
+        if(_isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else if(!_isPaused)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
