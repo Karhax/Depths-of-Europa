264b88ad749104dacce77869e9aaabab88b086bf
Erik <karhax@gmail.com>
2019-02-06 15:05:43 +0100

    Moved fish blinking

--- Depths of Europa/Assets/Design/Scripts/FishBlink.cs
+++ Depths of Europa/Assets/Design/Scripts/FishBlink.cs
@@ -15,6 +15,7 @@ public class FishBlink : MonoBehaviour {
 		Material mat = renderer.material;
 
 		float emission = Mathf.PingPong (Time.time, 1.0f);
+
 		Color baseColor = Color.white; //Replace this with whatever you want for your base color at emission level '1'
 
 		Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
