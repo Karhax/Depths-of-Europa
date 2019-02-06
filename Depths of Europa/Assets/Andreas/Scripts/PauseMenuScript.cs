de6cf459d01054aa09279fed004d49a6a98796b1
Andreas Söderlund <lets.play.ksp@gmail.com>
2019-02-06 12:30:46 +0100

    Finished First Pass Pause Menu Implimentation

--- Depths of Europa/Assets/Andreas/Scripts/PauseMenuScript.cs
+++ Depths of Europa/Assets/Andreas/Scripts/PauseMenuScript.cs
@@ -9,6 +9,10 @@ public class PauseMenuScript : MonoBehaviour {
     bool _isPaused = false;
     [SerializeField] GameObject _pauseMenuBackdrop;
 
+    public delegate void PauseStateHandler(bool pauseState);
+
+    public event PauseStateHandler PauseState;
+
     void Update () {
 		if(Input.GetButtonDown(GameInput.CANCEL))
         {
@@ -24,13 +28,48 @@ public class PauseMenuScript : MonoBehaviour {
         {
             if(_pauseMenuBackdrop != null)
                 _pauseMenuBackdrop.SetActive(true);
+
             Time.timeScale = 0;
+
+            if (PauseState != null)
+                PauseState(_isPaused);
         }
         else if(!_isPaused)
         {
             if (_pauseMenuBackdrop != null)
                 _pauseMenuBackdrop.SetActive(false);
+
             Time.timeScale = 1;
+
+            if (PauseState != null)
+                PauseState(_isPaused);
+        }
+    }
+
+    public void ResumeFromGUI()
+    {
+        SetPause();
+    }
+    //TODO: Impliment setting menu and settings menu trasition
+    public void OpenSettings()
+    {
+        Debug.Log("Settings menu and settings menu opening not implimented");
+    }
+    //TODO: Impliment main menu and main menu transition
+    public void ExitToMenu()
+    {
+        Debug.Log("Main menu and main menu trasition not implimented");
+    }
+
+    public void ExitToDesktop()
+    {
+        if (Application.isEditor)
+        {
+            Debug.Log("Not possible to exit to desktop wgile running in editor");
+        }
+        else
+        {
+            Application.Quit();
         }
     }
 }
