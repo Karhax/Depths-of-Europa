e5551c9a7f9f759a668565a3ac0f353aebec2498
Andreas Söderlund <lets.play.ksp@gmail.com>
2019-02-07 12:59:21 +0100

    Flares

---
+++ Depths of Europa/Assets/Andreas/Scripts/GameScripts/PlayerUI/FlareArrayScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FlareArrayScript : MonoBehaviour {
+    
+    [SerializeField] int _arrayID;
+    [SerializeField] GameObject[] _imageArray = new GameObject[5];
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
