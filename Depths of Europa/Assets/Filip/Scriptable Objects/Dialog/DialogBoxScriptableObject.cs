d28c7ce5882e6161daa7eede0d287dd5fa84ddbb
Filip Petersson <a18filpe@student.his.se>
2019-02-07 11:39:04 +0100

    Beginning of Dialog System, merge problems, easier for PERIBOI

---
+++ Depths of Europa/Assets/Filip/Scriptable Objects/Dialog/DialogBoxScriptableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialog Box", menuName = "Dialog/Dialog Box")]
+public class DialogBoxScriptableObject : ScriptableObject
+{
+    [SerializeField] DialogBoxObject[] _dialogBoxes;
+
+    public DialogBoxObject[] DialogBoxes { get; private set; }
+}
+
+[System.Serializable]
+public class DialogBoxObject
+{
+    [SerializeField, TextAreaAttribute(4, 4)] string _dialogText;
+    [SerializeField] Sprite _icon;
+    [SerializeField] Font _font;
+
+    public string DialogText { get; private set; }
+    public string Icon { get; private set; }
+    public Font Font { get; private set; }
+}
---
+++ Depths of Europa/Assets/Filip/Scripts/Dialog/StartDialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Statics;
+
+public class StartDialog : MonoBehaviour
+{
+    [Header("Settings")]
+
+    [SerializeField] float _minDialogTime;
+
+    [Header("Drop")]
+
+    [SerializeField] DialogBoxScriptableObject _thisDialogBoxScriptableObject;
+
+    Timer _minDialogTimer;
+
+    private void Awake()
+    {
+        _minDialogTimer = new Timer(_minDialogTime);
+    }
+
+    public void StartAllDialogs()
+    {
+
+    }
+
+    IEnumerator DoDialogBox(DialogBoxObject dialogObject)
+    {
+        yield return new WaitForEndOfFrame();
+    }
+}
