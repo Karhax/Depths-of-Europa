64bd603406d1be222e6d91bcc985f6b957ccb2c2
Andreas Söderlund <lets.play.ksp@gmail.com>
2019-02-07 11:48:55 +0100

    Start of UI Implimentation

---
+++ Depths of Europa/Assets/Andreas/Scripts/GameScripts/PlayerGUIScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerGUIScript : MonoBehaviour {
+
+    [SerializeField] Image _healthBarImage;
+    [SerializeField] float _debugCurrentHP = 100;
+    float _debugMaxHP = 100;
+    [SerializeField] bool _debugHPChangeCall = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_debugHPChangeCall)
+        {
+            OnHealthChange(_debugCurrentHP / _debugMaxHP);
+            _debugHPChangeCall = false;
+        }
+	}
+
+    private void OnHealthChange(float HPRatio)
+    {
+        _healthBarImage.fillAmount = HPRatio;
+    }
+}
