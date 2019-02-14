21142618fb2d1eb7c06b72be131ebfe989b9a64f
Zmarfan <47211326+Zmarfan@users.noreply.github.com>
2019-02-14 22:45:42 +0100

    Chaser done, Added Yoshi, fixed base texture fault

--- Depths of Europa/Assets/Filip/Scripts/Fish/SpawnChaser.cs
+++ Depths of Europa/Assets/Filip/Scripts/Fish/SpawnChaser.cs
@@ -8,17 +8,26 @@ public class SpawnChaser : MonoBehaviour
     [Header("Settings")]
 
     [SerializeField, Range(0.1f, 10f)] float _timeInLightBeforeAggro;
+    [SerializeField, Range(0, 5)] float _startPositionBack;
 
     [Header("Drop")]
 
     [SerializeField] GameObject _chaserFish;
-
+    [SerializeField] Transform _sprite;
+   
     Transform _fish;
     Transform _player;
     float _currentAggroTime = 0f;
     bool _inLight = false;
+    Vector3 _startPosition;
 
     bool _spawned = false;
+
+    private void Awake()
+    {
+        _startPosition = _sprite.localPosition - new Vector3(_startPositionBack, 0, 0);
+        _sprite.localPosition = _startPosition;
+    }
 
     private void Start()
     {
@@ -39,15 +48,19 @@ public class SpawnChaser : MonoBehaviour
 
     private void Update()
     {
-        if (_inLight)
+        if (_inLight && !_spawned)
         {
-            if (_currentAggroTime < _timeInLightBeforeAggro)
-                _currentAggroTime += Time.deltaTime;
-
             RaycastHit2D hit = Physics2D.Raycast(transform.position, _player.position - transform.position, Vector2.Distance(transform.position, _player.position), LayerMask.GetMask(Layers.DEFAULT));
 
-            if (hit.collider == null && _currentAggroTime >= _timeInLightBeforeAggro && !_spawned)
+            if (hit.collider == null)
+            {
+                _currentAggroTime += Time.deltaTime;
+
+                _sprite.localPosition = _startPosition + new Vector3(_currentAggroTime / _timeInLightBeforeAggro * _startPositionBack, 0, 0);
+
+                if (_currentAggroTime >= _timeInLightBeforeAggro)
                     SpawnFish();
+            }   
         }
         else if (_currentAggroTime > 0)
             _currentAggroTime -= Time.deltaTime;
@@ -72,10 +85,13 @@ public class SpawnChaser : MonoBehaviour
         _fish = (Instantiate(_chaserFish, transform.position, Quaternion.identity) as GameObject).transform;
         _fish.right = GameManager.ShipObject.transform.position - _fish.position;
         _fish.GetComponent<BasicChaserFish0>().FishOffScreenEvent += ResetSpawn;
+
+        _sprite.gameObject.SetActive(false);
     }
 
     private void ResetSpawn()
     {
+        _sprite.gameObject.SetActive(true);
         Destroy(_fish.gameObject);
         _spawned = false;
         _currentAggroTime = 0;
