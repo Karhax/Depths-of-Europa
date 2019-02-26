209839936bc799757de5ae6c2bed5b341e8f5588
Per Bengtsson <a14perbe@student.his.se>
2019-02-26 12:06:51 +0100

    Started work on improving the BigFishEvent after feedback Changes in original script mostly done
    Signed-off-by: Per Bengtsson <a14perbe@student.his.se>

--- Depths of Europa/Assets/Per/Scripts/BigFishShadowEvent.cs
+++ Depths of Europa/Assets/Per/Scripts/BigFishShadowEvent.cs
@@ -4,49 +4,44 @@ using UnityEngine;
 
 public class BigFishShadowEvent : MonoBehaviour {
 
-    [SerializeField] private Transform _bigFishShadow;
+    [SerializeField] private Sprite _bigFishSprite;
+    [SerializeField] private GameObject _bigFishPrefab;
     [SerializeField] [Range(0.1f, 10)] private float _moveSpeed = 2;
-    [SerializeField] [Range(-5, 5)] private float _turnSpeed = 0;
-    [SerializeField] [Range(1, 100)] private float _travelTimeDuration = 50;
+    [SerializeField] [Range(180, -180)] private float _startingAngle = 0;
+    [SerializeField] [Range(-10, 10)] private float _turnSpeed = 0;
     [SerializeField] [Range(-64, 64)] private float _offsetX = 0;
     [SerializeField] [Range(-64, 64)] private float _offsetY = 0;
-    
-    private bool _moving = false;
-    private Timer _travelTimer;
 
-    private void Awake()
-    {
-        _travelTimer = new Timer(_travelTimeDuration);
-    }
+    private GameObject _bigFishShadow = null;
+    private bool _moving = false;
 
     private void Update()
     {
-        if (_moving)
+        if (_moving && _bigFishShadow != null)
         {
-            _bigFishShadow.Rotate(Vector3.forward, _turnSpeed * Time.deltaTime * -1);
+            _bigFishShadow.transform.Rotate(Vector3.forward, _turnSpeed * Time.deltaTime * -1);
 
-            float angle = _bigFishShadow.localEulerAngles.z;
+            float angle = _bigFishShadow.transform.localEulerAngles.z;
             float x = Mathf.Sin(angle * Mathf.PI / 180) * _moveSpeed * -1;
             float y = Mathf.Cos(angle * Mathf.PI / 180) * _moveSpeed;
             Vector3 movePerFrame = new Vector3(x, y, 0);
 
-            _bigFishShadow.localPosition += movePerFrame * Time.deltaTime;
-            
-            _travelTimer.Time += Time.deltaTime;
-            if (_travelTimer.Expired())
-            {
-                Destroy(this.transform.parent.gameObject);
-            }
+            _bigFishShadow.transform.localPosition += movePerFrame * Time.deltaTime;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag(Statics.Tags.PLAYER_OUTSIDE))
+        if (other.gameObject.CompareTag(Statics.Tags.PLAYER_OUTSIDE) && _bigFishShadow == null)
         {
             _moving = true;
-            Vector3 location = new Vector3(other.transform.position.x + _offsetX, other.transform.position.y + _offsetY, _bigFishShadow.transform.position.z);
-            _bigFishShadow.transform.position = location;
+            Vector3 location = new Vector3(other.transform.position.x + _offsetX, other.transform.position.y + _offsetY, this.transform.position.z);
+            _bigFishShadow = Instantiate(_bigFishPrefab, location, Quaternion.Euler(0, 0, _startingAngle));
         }
+    }
+
+    private void RemoveEvent()
+    {
+
     }
 }
