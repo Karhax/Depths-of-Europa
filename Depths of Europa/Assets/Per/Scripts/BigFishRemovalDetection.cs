073ef7b95c938b01f4346484cb7ca0e3b5202801
Per Bengtsson <a14perbe@student.his.se>
2019-02-26 13:34:02 +0100

    BigFishEvent now fulfills all criteria that were set during feedback
    Signed-off-by: Per Bengtsson <a14perbe@student.his.se>

---
+++ Depths of Europa/Assets/Per/Scripts/BigFishRemovalDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigFishRemovalDetection : MonoBehaviour {
+
+    public delegate void AreaExitDelegate();
+    public event AreaExitDelegate ActiveAreaExit;
+
+	private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Statics.Tags.IN_CAMERA_TRIGGER))
+        {
+            if(ActiveAreaExit != null)
+            {
+                ActiveAreaExit();
+            }
+        }
+    }
+}
