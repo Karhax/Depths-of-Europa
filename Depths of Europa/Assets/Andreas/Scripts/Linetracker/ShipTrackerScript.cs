3782e36e00c1d91cb590f854af2cb898e1126743
Andreas Söderlund <lets.play.ksp@gmail.com>
2019-02-27 11:47:10 +0100

    added bad solution

--- Depths of Europa/Assets/Andreas/Scripts/Linetracker/ShipTrackerScript.cs
+++ Depths of Europa/Assets/Andreas/Scripts/Linetracker/ShipTrackerScript.cs
@@ -2,26 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipTrackerScript : MonoBehaviour {
+public class ShipTrackerScript : MonoBehaviour
+{
 
     [SerializeField, Range(0.1f, 2)] float _pollingIntervall = 0.5f;
     [SerializeField, Range(0.25f, 5)] float _distanceThreashold = 1;
+    [SerializeField, Range(1, 10)] float _maxLineWidth;
 
     float _pollingCounter, _lastTime = 0;
-	// Use this for initialization
-	void Start () {
+    Vector2 _lastPosition;
+    // Use this for initialization
+    void Start()
+    {
         _lastTime = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        _lastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         _pollingCounter += Time.deltaTime;
-        if(_pollingCounter >= _pollingIntervall)
+        if (_pollingCounter >= _pollingIntervall)
         {
             _pollingCounter = 0;
+            float currentDistance = Vector2.Distance(transform.position, _lastPosition);
+            if (currentDistance <= _distanceThreashold)
+            {
+                float timeThiccness = Time.time - _lastTime / currentDistance;
+                if (timeThiccness >= _maxLineWidth)
+                {
+
+                }
+
+
+            }
 
         }
-
-	}
+    }
 }
