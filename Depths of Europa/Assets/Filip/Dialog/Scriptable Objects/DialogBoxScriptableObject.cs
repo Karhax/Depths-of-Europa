b8e98d9a96d3e5e88ffb56518fde938ff96e5151
Zmarfan <47211326+Zmarfan@users.noreply.github.com>
2019-03-07 00:50:53 +0100

    Dialog effects started

--- Depths of Europa/Assets/Filip/Dialog/Scriptable Objects/DialogBoxScriptableObject.cs
+++ Depths of Europa/Assets/Filip/Dialog/Scriptable Objects/DialogBoxScriptableObject.cs
@@ -2,6 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public abstract class DialogEffectBase : ScriptableObject
+{
+    public abstract void Update();
+}
+
+[CreateAssetMenu(fileName = "New DialogEffectColor", menuName = "Dialog/Effect/Color")]
+public class DialogEffectColor : DialogEffectBase
+{
+    [SerializeField] Color _effectColor1 = Color.white;
+    [SerializeField] Color _effectColor2 = Color.white;
+    [SerializeField] float _changeColorSpeed = 5;
+
+    public override void Update()
+    {
+    }
+}
+
+[CreateAssetMenu(fileName = "New DialogEffectShake", menuName = "Dialog/Effect/Shake")]
+public class DialogEffectShake : DialogEffectBase
+{
+    [SerializeField] float _shakeSpeed;
+
+    public override void Update()
+    {
+    }
+}
+
 [CreateAssetMenu(fileName = "New Dialog Box", menuName = "Dialog/Dialog Box")]
 public class DialogBoxScriptableObject : ScriptableObject
 {
@@ -18,6 +46,7 @@ public class DialogBoxObject
     [SerializeField] CharacterScriptableObject _leftCharacter;
     [SerializeField, TextAreaAttribute(3, 3)] string _dialogText;
     [SerializeField] AudioClip _textAudio;
+    [SerializeField] DialogEffectBase _effect;
 
     public bool RightTalking { get { return _rightTalking; } private set { _rightTalking = value; } }
     public CharacterScriptableObject RightCharacter { get { return _rightCharacter; } private set { _rightCharacter = value; } }
