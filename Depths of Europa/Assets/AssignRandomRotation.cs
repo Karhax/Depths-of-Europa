29e2b6aa3ab584e098c30ed9cee91efc4a28671f
Joacim Orlov <a18joaor@student.his.se>
2019-03-21 12:15:39 +0100

    level 1 + random rotaion & random scale script
    Signed-off-by: Joacim Orlov <a18joaor@student.his.se>

---
+++ Depths of Europa/Assets/AssignRandomRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+
+public class AssignRandomRotation : MonoBehaviour
+{
+
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, Random.Range(-180, 180));
+    }
+
+
+}
---
+++ Depths of Europa/Assets/AssignRandomScale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+
+public class AssignRandomScale : MonoBehaviour
+{
+    private float scale;
+
+    // Use this for initialization
+    void Awake()
+    {
+        scale = Random.Range(0.1f, 0.5f);
+        transform.localScale = new Vector3(scale, scale, 0);
+    }
+
+
+}
