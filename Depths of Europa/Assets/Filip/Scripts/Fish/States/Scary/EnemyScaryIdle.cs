3534820c95d647e580cc9c502c6f6e705621e2e5
Zmarfan <47211326+Zmarfan@users.noreply.github.com>
2019-03-26 23:00:20 +0100

    Level 3 fix och tricks top 10 coolest anime moments

--- Depths of Europa/Assets/Filip/Scripts/Fish/States/Scary/EnemyScaryIdle.cs
+++ Depths of Europa/Assets/Filip/Scripts/Fish/States/Scary/EnemyScaryIdle.cs
@@ -10,10 +10,10 @@ public class EnemyScaryIdle : EnemyIdleBase
     {
         if (other.CompareTag(Tags.LIGHT) || other.CompareTag(Tags.FLARE_TRIGGER))
             return ShouldAttack(other.transform.position);
-        else if (other.CompareTag(Tags.NOTICE_HIGH_SPEED) && _noticeByHighSpeed)
-            return ShouldAttack(other.transform.position);
-        else if (other.CompareTag(Tags.NOTICE_LOW_SPEED) && !_noticeByHighSpeed)
-            return ShouldAttack(other.transform.position);
+        //else if (other.CompareTag(Tags.NOTICE_HIGH_SPEED) && _noticeByHighSpeed)
+        //    return ShouldAttack(other.transform.position);
+        //else if (other.CompareTag(Tags.NOTICE_LOW_SPEED) && !_noticeByHighSpeed)
+        //    return ShouldAttack(other.transform.position);
         else if (other.CompareTag(Tags.WALL) || other.CompareTag(Tags.ICE))
             BackUp(_thisTransform.position - other.transform.position);
         else if (other.CompareTag(Tags.PLAYER_OUTSIDE) || other.CompareTag(Tags.ALL_FISH_ESCAPE))
